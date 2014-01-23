cfac836304ae7772cd12f50dc0f3974be79d2418
francois romain <francoisromain@gmail.com>
2014-01-23 15:31:01 +0100

    corrected toaster bug

--- app-dev/js/services.js
+++ app-dev/js/services.js
@@ -4,91 +4,91 @@
 
 angular.module('clearApp.services', ['ngResource'])
 	.factory('E1', ['$resource', function($resource) {
-	    return $resource('../index_rest.php/api/clear/v1/:type/:id', { type:'@type', id:'@id' });
+		return $resource('../index_rest.php/api/clear/v1/:type/:id', { type:'@type', id:'@id' });
 	}])
 	.factory('Elements', ['$resource', function($resource) {
-	    return $resource('../index_rest.php/api/clear/v2/:type/:id', 
-		    { 	type:'@type', id:'@id' },
-		    {	'rec': {method: 'POST', isArray:false }
-	    });
+		return $resource('../index_rest.php/api/clear/v2/:type/:id', 
+			{ 	type:'@type', id:'@id' },
+			{	'rec': {method: 'POST', isArray:false }
+		});
 	}])
 	.factory('Base64', function() {
-	    var keyStr = 'ABCDEFGHIJKLMNOP' + 'QRSTUVWXYZabcdef' + 'ghijklmnopqrstuv' + 'wxyz0123456789+/' + '=';
+		var keyStr = 'ABCDEFGHIJKLMNOP' + 'QRSTUVWXYZabcdef' + 'ghijklmnopqrstuv' + 'wxyz0123456789+/' + '=';
 		return {
-	        encode: function (input) {
-	            var output = "";
-	            var chr1, chr2, chr3 = "";
-	            var enc1, enc2, enc3, enc4 = "";
-	            var i = 0;
+			encode: function (input) {
+				var output = "";
+				var chr1, chr2, chr3 = "";
+				var enc1, enc2, enc3, enc4 = "";
+				var i = 0;
 	 
-	            do {
-	                chr1 = input.charCodeAt(i++);
-	                chr2 = input.charCodeAt(i++);
-	                chr3 = input.charCodeAt(i++);
+				do {
+					chr1 = input.charCodeAt(i++);
+					chr2 = input.charCodeAt(i++);
+					chr3 = input.charCodeAt(i++);
 	 
-	                enc1 = chr1 >> 2;
-	                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-	                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-	                enc4 = chr3 & 63;
+					enc1 = chr1 >> 2;
+					enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+					enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+					enc4 = chr3 & 63;
 	 
-	                if (isNaN(chr2)) {
-	                    enc3 = enc4 = 64;
-	                } else if (isNaN(chr3)) {
-	                    enc4 = 64;
-	                }
+					if (isNaN(chr2)) {
+						enc3 = enc4 = 64;
+					} else if (isNaN(chr3)) {
+						enc4 = 64;
+					}
 	 
-	                output = output +
-	                    keyStr.charAt(enc1) +
-	                    keyStr.charAt(enc2) +
-	                    keyStr.charAt(enc3) +
-	                    keyStr.charAt(enc4);
-	                chr1 = chr2 = chr3 = "";
-	                enc1 = enc2 = enc3 = enc4 = "";
-	            } while (i < input.length);
-	            return output;
-	        },
+					output = output +
+						keyStr.charAt(enc1) +
+						keyStr.charAt(enc2) +
+						keyStr.charAt(enc3) +
+						keyStr.charAt(enc4);
+					chr1 = chr2 = chr3 = "";
+					enc1 = enc2 = enc3 = enc4 = "";
+				} while (i < input.length);
+				return output;
+			},
 	 
-	        decode: function (input) {
-	            var output = "";
-	            var chr1, chr2, chr3 = "";
-	            var enc1, enc2, enc3, enc4 = "";
-	            var i = 0;
+			decode: function (input) {
+				var output = "";
+				var chr1, chr2, chr3 = "";
+				var enc1, enc2, enc3, enc4 = "";
+				var i = 0;
 	 
-	            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-	            var base64test = /[^A-Za-z0-9\+\/\=]/g;
-	            if (base64test.exec(input)) {
-	                alert("There were invalid base64 characters in the input text.\n" +
-	                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-	                    "Expect errors in decoding.");
-	            }
-	            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+				// remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+				var base64test = /[^A-Za-z0-9\+\/\=]/g;
+				if (base64test.exec(input)) {
+					alert("There were invalid base64 characters in the input text.\n" +
+						"Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+						"Expect errors in decoding.");
+				}
+				input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 	 
-	            do {
-	                enc1 = keyStr.indexOf(input.charAt(i++));
-	                enc2 = keyStr.indexOf(input.charAt(i++));
-	                enc3 = keyStr.indexOf(input.charAt(i++));
-	                enc4 = keyStr.indexOf(input.charAt(i++));
+				do {
+					enc1 = keyStr.indexOf(input.charAt(i++));
+					enc2 = keyStr.indexOf(input.charAt(i++));
+					enc3 = keyStr.indexOf(input.charAt(i++));
+					enc4 = keyStr.indexOf(input.charAt(i++));
 	 
-	                chr1 = (enc1 << 2) | (enc2 >> 4);
-	                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-	                chr3 = ((enc3 & 3) << 6) | enc4;
+					chr1 = (enc1 << 2) | (enc2 >> 4);
+					chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+					chr3 = ((enc3 & 3) << 6) | enc4;
 	 
-	                output = output + String.fromCharCode(chr1);
+					output = output + String.fromCharCode(chr1);
 	 
-	                if (enc3 != 64) {
-	                    output = output + String.fromCharCode(chr2);
-	                }
-	                if (enc4 != 64) {
-	                    output = output + String.fromCharCode(chr3);
-	                }
+					if (enc3 != 64) {
+						output = output + String.fromCharCode(chr2);
+					}
+					if (enc4 != 64) {
+						output = output + String.fromCharCode(chr3);
+					}
 	 
-	                chr1 = chr2 = chr3 = "";
-	                enc1 = enc2 = enc3 = enc4 = "";
+					chr1 = chr2 = chr3 = "";
+					enc1 = enc2 = enc3 = enc4 = "";
 	 
-	            } while (i < input.length);
+				} while (i < input.length);
 	 		
-	            return output;
-	        }
+				return output;
+			}
 		};
 	})
 	.factory('Utils', function(){
@@ -102,61 +102,61 @@ angular.module('clearApp.services', ['ngResource'])
 			}, 
 			is_empty: function (obj) {
 				var hasOwnProperty = Object.prototype.hasOwnProperty;
-			    // null and undefined are empty
-			    if (obj == null) return true;
-			    // Assume if it has a length property with a non-zero value
-			    // that that property is correct.
-			    if (obj.length && obj.length > 0)    return false;
-			    if (obj.length === 0)  return true;
+				// null and undefined are empty
+				if (obj == null) return true;
+				// Assume if it has a length property with a non-zero value
+				// that that property is correct.
+				if (obj.length && obj.length > 0)    return false;
+				if (obj.length === 0)  return true;
 			
-			    for (var key in obj) {
-			        if (hasOwnProperty.call(obj, key))    return false;
-			    }
-			    // Doesn't handle toString and toValue enumeration bugs in IE < 9
-			    return true;
+				for (var key in obj) {
+					if (hasOwnProperty.call(obj, key))    return false;
+				}
+				// Doesn't handle toString and toValue enumeration bugs in IE < 9
+				return true;
 			}, 
 			IndxOf: function(myArray, searchTerm, property) {
-			    for(var i = 0, len = myArray.length; i < len; i++) {
-			        if (myArray[i][property] === searchTerm) return i+1;
-			    }
-			    return -1;
+				for(var i = 0, len = myArray.length; i < len; i++) {
+					if (myArray[i][property] === searchTerm) return i+1;
+				}
+				return -1;
 			}, 
 			collect: function (a,b){
-			    var c = {};
-			    for (var att in a) { c[att] = a[att]; }
-			    for (var att in b) { c[att] = b[att]; }
-			    return c;
+				var c = {};
+				for (var att in a) { c[att] = a[att]; }
+				for (var att in b) { c[att] = b[att]; }
+				return c;
 			}, 
 			clone: function (obj) {
-			    // Handle the 3 simple types, and null or undefined
-			    if (null == obj || "object" != typeof obj) return obj;
+				// Handle the 3 simple types, and null or undefined
+				if (null == obj || "object" != typeof obj) return obj;
 			
-			    // Handle Date
-			    if (obj instanceof Date) {
-			        var copy = new Date();
-			        copy.setTime(obj.getTime());
-			        return copy;
-			    }
+				// Handle Date
+				if (obj instanceof Date) {
+					var copy = new Date();
+					copy.setTime(obj.getTime());
+					return copy;
+				}
 			
-			    // Handle Array
-			    if (obj instanceof Array) {
-			        var copy = [];
-			        for (var i = 0, len = obj.length; i < len; i++) {
-			            copy[i] = clone(obj[i]);
-			        }
-			        return copy;
-			    }
+				// Handle Array
+				if (obj instanceof Array) {
+					var copy = [];
+					for (var i = 0, len = obj.length; i < len; i++) {
+						copy[i] = clone(obj[i]);
+					}
+					return copy;
+				}
 			
-			    // Handle Object
-			    if (obj instanceof Object) {
-			        var copy = {};
-			        for (var attr in obj) {
-			            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
-			        }
-			        return copy;
-			    }
+				// Handle Object
+				if (obj instanceof Object) {
+					var copy = {};
+					for (var attr in obj) {
+						if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+					}
+					return copy;
+				}
 			
-			    throw new Error("Unable to copy obj! Its type isn't supported.");
+				throw new Error("Unable to copy obj! Its type isn't supported.");
 			}
 		}
 	})
@@ -189,95 +189,110 @@ angular.module('clearApp.services', ['ngResource'])
 				return query;
 			}, 
 			modalOpen: function (elm, required, $event) {            
-	        	if ($event.stopPropagation) $event.stopPropagation();
+				if ($event.stopPropagation) $event.stopPropagation();
 				if (required.editable || !required.completed) {
-		            var modalInstance = $modal.open({
-		                templateUrl: 'partials/tplModal.html',
-		                controller: 'TplModalCtrl',
-		                resolve: {
-		                  required: function () {
-		                    return required;
-		                  },
-		                  elm: function () {
-		                    return elm;
-		                  }
-		                    
-		                }
-		            });
+					var modalInstance = $modal.open({
+						templateUrl: 'partials/tplModal.html',
+						controller: 'TplModalCtrl',
+						resolve: {
+						  required: function () {
+							return required;
+						  },
+						  elm: function () {
+							return elm;
+						  }
+							
+						}
+					});
 					modalInstance.result.then(function (selectedItem) {
 	//				    $scope.selected = selectedItem;
 					}, function () {
 	//                $log.info('Modal dismissed at: ' + new Date());
 					});
 				}
-	        }, 
-	        trackingToggle: function(elm, $event) {
-	        	if ($event.stopPropagation) $event.stopPropagation(); 
-	        	elm.tracking=!elm.tracking;
-	        }, 
-	        propertySave: function(elm, type, group) {
-	        	$rootScope.currentGroup = group;
+			}, 
+			trackingToggle: function(elm, $event) {
+				if ($event.stopPropagation) $event.stopPropagation(); 
+				elm.tracking=!elm.tracking;
+			}, 
+			propertySave: function(elm, type, group) {
+				$rootScope.currentGroup = group;
 				console.log('saving property');
-	        	elm.$save({update:type}, function(p, response) {
-	        	    if (response("X-Clear-updatedProperty")==="success") {
-	        	    	toaster.pop("success", "Updated property", response("X-Clear-updatedPropertyName"));
-	        	    } else if (response("X-Clear-updatedProperty")==="warning") {
-	        	    	toaster.pop("warning", "Property has not been updated", response("X-Clear-updatedPropertyName"));
-	        	    } else if (response("X-Clear-updatedProperty")==="error") {
-	        	    	toaster.pop("error", "Property has not been updated", response("X-Clear-updatedPropertyName"));
-	        	    }
-	        		console.log('saved-> elm: ', elm,'/ type: ', type,'/ group: ', group) 
-	        	});
-	        }, 
-	        requiredSave: function (elm, id) {
-	            elm.$save({update:'required', update_id:id}, function(p, response) {
-	            	if (response("X-Clear-updatedRequired")==="success") {
-	            		toaster.pop("success", "Updated required element", response("X-Clear-updatedRequiredName"));
-	            	} else if (response("X-Clear-updatedRequired")==="error") {
-	            		toaster.pop("error", "Required element has not been updated", response("X-Clear-updatedRequiredName"));
-	            	}
-	            	
-	            	if (response("X-Clear-updatedMilestone")==="success") {
-	            		toaster.pop("success", "Updated milestone", response("X-Clear-updatedMilestoneName"));
-	            	} else if (response("X-Clear-updatedMilestone")==="error") {
-	            		toaster.pop("error", "Milestone has not been updated", response("X-Clear-updatedMilestoneName"));
-	            	}
-	            	console.log('saved-> elm: ', elm,'/ type: required / update_id: ', id); 
-	            });
-	        }, 
-	        go: function (type, id, related) {
-	        	$rootScope.tvScreen = false;
-	        	$location.$$search = {};
-	        	$location.path( type + '/' + id )
-	        	if (related) {
-	        		$location.search('related_type', related).hash('related');
-	        	}
-	        	console.log('go ->', type, id, related);
-	        }, 
-	        propertiesDate: function (elm){
-	        	// init date properties from unix timestamp to javascript date
-	        	var date = {};
-	        	for (var group in elm.properties) {
-	        		for (var property in elm.properties[group].set) {
-	        			var property = elm.properties[group].set[property];
-	        			if (property.type === 'date') {
-	        				date[property.name] = {};
-	        				if (property.value)	date[property.name].value = Utils.timestampToDate(property.value);
-	        			}
-	        		}
-	        	}
-	        	return date;
-	        }, 
-	        qrCodeGoogle: function (elm) {
-	        	var e;
-	        	switch (elm.type) {
-	        		case 'order': e = 'O'; break;
-	        		case 'shipment': e = 'S'; break;
-	        		case 'box': e = 'B'; break;
-	        		case 'item': e = 'I'; break;
-	        	}
-	        	return 'http://chart.apis.google.com/chart?cht=qr&chs=63x63&chl='+ e + elm.id + '&chld=H%7C0';
-	        }
+				elm.$save({update:type}, function(p, response) {
+					var propertyMessage; 
+					
+					if (response("X-Clear-updatedProperty")==="success") {
+						propertyMessage = "Updated property";
+					} else if (response("X-Clear-updatedProperty")==="warning") {
+						propertyMessage = "Property has not been updated";
+					} else if (response("X-Clear-updatedProperty")==="error") {
+						propertyMessage = "Property has not been updated";
+					}
+					
+					if (response("X-Clear-updatedProperty")) toaster.pop(response("X-Clear-updatedProperty"), message, response("X-Clear-updatedPropertyName"));
+					console.log('saved-> elm: ', elm,'/ type: ', type,'/ group: ', group) 
+				});
+			}, 
+			requiredSave: function (elm, id) {
+				elm.$save({update:'required', update_id:id}, function(p, response) {
+					var requiredMessage, milestoneMessage; 
+					
+					if (response("X-Clear-updatedRequired")==="success") {
+						requiredMessage = "Updated condition";
+					} else if (response("X-Clear-updatedRequired")==="warning") {
+						propertyMessage = "Condition has not been updated";
+					} else if (response("X-Clear-updatedRequired")==="error") {
+						requiredMessage = "Condition element has not been updated"
+					}
+					
+					if (response("X-Clear-updatedMilestone")==="success") {
+						milestoneMessage = "Updated milestone";
+					} else if (response("X-Clear-updatedMilestone")==="warning") {
+						propertyMessage = "Milestone has not been updated";
+					} else if (response("X-Clear-updatedMilestone")==="error") {
+						milestoneMessage = "Milestone has not been updated";
+					}
+					
+					
+					if (response("X-Clear-updatedRequired")) toaster.pop(response("X-Clear-updatedRequired"), requiredMessage, response("X-Clear-updatedRequiredName"));
+					if (response("X-Clear-updatedMilestone")) toaster.pop(response("X-Clear-updatedMilestone"), milestoneMessage, response("X-Clear-updatedMilestoneName"));
+					
+					console.log('saved-> elm: ', elm,'/ type: required / update_id: ', id); 
+				});
+			}, 
+			go: function (type, id, related) {
+				$rootScope.tvScreen = false;
+				$location.$$search = {};
+				$location.path( type + '/' + id )
+				if (related) {
+					$location.search('related_type', related).hash('related');
+				}
+				console.log('go ->', type, id, related);
+			}, 
+			propertiesDate: function (elm){
+				// init date properties from unix timestamp to javascript date
+				var date = {};
+				for (var group in elm.properties) {
+					for (var property in elm.properties[group].set) {
+						var property = elm.properties[group].set[property];
+						if (property.type === 'date') {
+							date[property.name] = {};
+							if (property.value)	date[property.name].value = Utils.timestampToDate(property.value);
+						}
+					}
+				}
+				return date;
+			}, 
+			qrCodeGoogle: function (elm) {
+				var e;
+				switch (elm.type) {
+					case 'order': e = 'O'; break;
+					case 'shipment': e = 'S'; break;
+					case 'box': e = 'B'; break;
+					case 'item': e = 'I'; break;
+				}
+				return 'http://chart.apis.google.com/chart?cht=qr&chs=63x63&chl='+ e + elm.id + '&chld=H%7C0';
+			}
 		}
 	}])
 	.factory('ColorScaleConfig', function(){
@@ -325,62 +340,62 @@ angular.module('clearApp.services', ['ngResource'])
 			colors: { 
 				brand: function() {
 					return function(d, i) {
-				    	return colorArrayBrand[i];
-				    };
+						return colorArrayBrand[i];
+					};
 				},
 				order: function() {
 					return function(d, i) {
-				    	return colorArrayOrder[i];
-				    };
+						return colorArrayOrder[i];
+					};
 				},
 				shipment: function() {
 					return function(d, i) {
-				    	return colorArrayShipment[i];
-				    };
+						return colorArrayShipment[i];
+					};
 				},
 				box: function() {
 					return function(d, i) {
-				    	return colorArrayBox[i];
-				    };
+						return colorArrayBox[i];
+					};
 				},
 				item: function() {
 					return function(d, i) {
-				    	return colorArrayItem[i];
-				    };
+						return colorArrayItem[i];
+					};
 				}
 			}, 
 			tooltips: {
 				pieChart: function(){
 					return function(key, x, y, e, graph) {
-				    	return  '<h6>' + key + '</h6>' + '<p>' + x + '</p>'
+						return  '<h6>' + key + '</h6>' + '<p>' + x + '</p>'
 					}
 				},
 				hBarChart: function(){
 					return function(key, x, y, e, graph) {
-				    	return  '<h6>' + key + '</h6>' + '<p>' +  y + '</p>'
+						return  '<h6>' + key + '</h6>' + '<p>' +  y + '</p>'
 					}
 				},
 				vBarChart: function(){
 					return function(key, x, y, e, graph) {
-				    	return  '<h6>' + x + ' / ' + key + '</h6>' + '<p>' +  y + '</p>'
+						return  '<h6>' + x + ' / ' + key + '</h6>' + '<p>' +  y + '</p>'
 					}
 				}
 			}, 
 			chartFn: function (datas) {
 				datas.xFunction = function(){
-				    return function(d) {
-				        return d.key;
-				    };
+					return function(d) {
+						return d.key;
+					};
 				}
 				datas.yFunction = function(){
-				    return function(d) {
-				        return d.value;
-				    };
+					return function(d) {
+						return d.value;
+					};
 				}
 				datas.descriptionFunction = function(){
-				    return function(d){
-				        return d.key;
-				    }
+					return function(d){
+						return d.key;
+					}
 				}
 			}
 		}
